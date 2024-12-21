84c2410b0966bae4a9493640d427884d2a7f80dc
Navneet Shahi <navneetshahi345@gmail.com>
2024-12-21 20:33:14 +0530

    update 5 files

---
+++ app/create/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CreateStoryPage = () => {
+  return (
+    <div>
+      <h1>CreateStoryPage</h1>
+    </div>
+  );
+};
+
+export default CreateStoryPage;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Header from "./_components/Header";
+import Hero from "./_components/Hero";
 
 export default function Home() {
   return (
     <div className="bg-[#cad3ff]">
       <Header />
+      <Hero />
     </div>
   );
 }
